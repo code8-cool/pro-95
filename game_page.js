c9a5e35e67c99ac741828ff9dbc96714cbd474eb
code8-cool <136864069+code8-cool@users.noreply.github.com>
2023-11-03 21:19:14 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,7 +1,5 @@
 
-	//Get "player1_name" using localStorage.getItem() function and assign it to player1_name
-	//Get "player2_name" using localStorage.getItem() function and assign it to player2_name
-	localStorage.getItem("player1_name")
+		localStorage.getItem("player1_name")
 	localStorage.getItem("player2_name")
 
 	player1_score = 0;
